755d2ee9866eef30336fa56b9ecad10591085c6d
suren <zxjlwt@126.com>
2018-06-01 19:12:53 +0800

    add tycho plugin

--- src/io/jenkins/x/eclipse/plugin/Startup.java
+++ src/io/jenkins/x/eclipse/plugin/Startup.java
@@ -4,14 +4,6 @@
 package io.jenkins.x.eclipse.plugin;
 
 import org.eclipse.ui.IStartup;
-import org.eclipse.ui.console.MessageConsole;
-import org.eclipse.ui.console.MessageConsoleStream;
-
-import io.fabric8.kubernetes.api.model.ObjectMeta;
-import io.fabric8.kubernetes.client.KubernetesClientException;
-import io.fabric8.kubernetes.client.Watcher;
-import io.jenkins.x.client.PipelineClient;
-import io.jenkins.x.client.kube.PipelineActivity;
 
 /**
  * @author suren
@@ -21,31 +13,5 @@ public class Startup implements IStartup {
 
 	@Override
 	public void earlyStartup() {
-//		PipelineClient client = PipelineClient.newInstance();
-//		client.start();
-//		
-//		final MessageConsole console = JenkinsConsole.getConsole();
-//		MessageConsoleStream msg = console.newMessageStream();
-//		
-//		client.addListener(new Watcher<PipelineActivity>() {
-//
-//			@Override
-//			public void eventReceived(Action action, PipelineActivity activity) {
-//				ObjectMeta metadata = activity.getMetadata();
-//				
-//				String clusterName = metadata.getClusterName();
-//				String namespace = metadata.getNamespace();
-//				String name = metadata.getName();
-//				
-//				msg.println("Cluster: " + clusterName + " Namespace:" + namespace + " " + name + " action: " + action);
-//			}
-//
-//			@Override
-//			public void onClose(KubernetesClientException e) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//			
-//		});
 	}
 }
